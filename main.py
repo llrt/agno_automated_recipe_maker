8d9f0e05098195a2c8c844547aff6149cca461f7
llrt <leandroloriato@gmail.com>
2025-09-11 17:19:46 -0300

    Adding Agno's quickstart, to test setup

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from agno-automated-recipe-maker!")
+from agno.agent import Agent
+from agno.models.google import Gemini
+from dotenv import load_dotenv
 
+load_dotenv() # Read env vars from .env file
+print("Env vars loaded..")
 
-if __name__ == "__main__":
-    main()
+# Define LLM model
+MODEL_ID = "gemini-2.5-flash"
+
+# Using Google AI Studio
+agent = Agent(
+    model=Gemini(id=MODEL_ID),
+    markdown=True,
+)
+
+# Print the response in the terminal
+agent.print_response("Share a 2 sentence horror story.")
